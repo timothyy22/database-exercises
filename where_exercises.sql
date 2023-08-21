23a287af6286201cc30ce0f0c3efad9e9bc932f5
timothy <timothywsingletary@gmail.com>
2023-08-21 10:01:36 -0500

    where-exercise

---
+++ where_exercises.sql
@@ -0,0 +1,27 @@
+USE employees;
+
+SELECT emp_no, first_name, last_name
+FROM employees.employees
+WHERE first_name IN ('Irena', 'Vidya','Maya')
+AND gender = 'M';
+
+
+SELECT emp_no, first_name, last_name
+FROM employees.employees
+WHERE LEFT (last_name, 1) = 'E'
+OR RIGHT(last_name, 1) = 'e';
+
+SELECT emp_no, first_name, last_name
+FROM employees.employees
+WHERE LEFT (last_name, 1) = 'E'
+AND RIGHT(last_name, 1) = 'e';
+
+
+SELECT emp_no, first_name, last_name
+FROM employees.employees
+WHERE last_name LIKE '%q%';
+
+SELECT emp_no, first_name, last_name
+FROM employees.employees
+WHERE last_name LIKE '%q%'
+and last_name NOT LIKE '%qu%'
